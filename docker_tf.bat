d9fb86b9691246c4c1211ded5ce632c61ea92be4
Dylan Praul <dylan.praul@gmail.com>
2016-08-29 01:33:53 -0400

    Remove hard-coded directory from docker_tf

--- docker_tf.bat
+++ docker_tf.bat
@@ -1,1 +1,7 @@
-docker run -it -v /c/projects/genres-are-cool:/notebooks tf python run.py %1 %2 %3
+@ECHO off
+SET _dir=/%CD%
+SET _dir=%_dir::=%
+SET _dir=%_dir:\=/%
+@ECHO on
+
+docker run -it -v %_dir%:/notebooks tf python run.py %1 %2 %3
